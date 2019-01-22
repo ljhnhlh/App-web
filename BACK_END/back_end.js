79e7bcc7f2adcf55a0a1131b76119b5cbb5e16a3
ljhnhlh <1292993785@qq.com>
2019-01-22 19:46:25 +0800

    use express and solve the post problem

--- BACK_END/back_end.js
+++ BACK_END/back_end.js
@@ -1,62 +1,26 @@
-// 导入http模块:
-var http = require('http'); //http 模块
-var url = require('url'); //地址模块
-var fs = require('fs'); //文件读取模块
-var path = require('path'); //路径模块
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
 
-var server = http.createServer(function(req, res) {
-    console.log(req.method + ":" + req.url);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    var pathname = url.parse(req.url).path;
-    console.log(pathname);
-    if (pathname === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        var filepath = path.join('..', '/HTML/', 'signUp.html');
-        console.log(filepath);
-
-        fs.stat(filepath, function(err, stats) {
-            if (!err && stats.isFile()) {
-                console.log('200 ' + req.url);
-                res.writeHead(200);
-                fs.createReadStream(filepath).pipe(res);
-            } else {
-                console.log('404 ' + req.url);
-                res.writeHead(404);
-                res.end('404 Not Found');
-            }
-        });
-    } else if (req.method === 'POST') {
-        req.addListener('data', function(chunk) {
-            console.log(JSON.parse(chunk));
-
-        });
-        req.addListener('end', function() {
-            res.end('success');
-        });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        var filepath = path.join('..', pathname);
-        console.log(filepath);
-
-        // //错的
-        // console.log("name :" + req.body.name + " password :" + req.body.password);
-
-        console.log(req.body);
-
-        fs.stat(filepath, function(err, stats) {
-            if (!err && stats.isFile()) {
-                console.log('200 ' + req.url);
-                res.writeHead(200);
-                fs.createReadStream(filepath).pipe(res);
-            } else {
-                console.log('404 ' + req.url);
-                res.writeHead(404);
-                res.end('404 Not Found');
-            }
-        });
-    }
-
+app.get('/', function(req, res) {
+    console.log(req.path);
+    res.sendFile('E:\\APP\\HTML\\signUp.html');
 });
-server.listen(8080);
-console.log('Server is running at http://127.0.0.1:8080/');
+app.get('*', function(req, res) {
+    console.log(req.url);
+    res.sendFile('E:/APP/' + req.path);
+})
+app.post('*', function(req, res) {
+    console.log(req.url);
+    var temp = req.body;
+    console.log(temp);
+    res.sendFile('E:/APP' + req.path);
+})
+
+app.listen(8080);
+console.log('the server running at http://127.0.0.1:8080/');
--- Learning/Learning.md
+++ Learning/Learning.md
@@ -1,3 +1,20 @@
+
+
+
+
+# 目录
+
+[TOC]
+
 # 引入 jquery
+
 与引入一般的js文件方法一致
 `<script src="/JS/jquery-3.3.1.js"></script>`
+
+
+
+# HTML 引入css
+
+`<link rel="stylesheet" type="text/css" href="/CSS/index.css">
+
+ `
